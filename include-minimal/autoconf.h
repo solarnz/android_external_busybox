ad5c7644ff99a710b86eb3b056e0e515b93c8e97
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-05-22 13:34:17 +0200

    update config date

--- include-minimal/autoconf.h
+++ include-minimal/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.19.0-cm71
  */
-#define AUTOCONF_TIMESTAMP "2011-05-20 00:00:00 CEST"
+#define AUTOCONF_TIMESTAMP "2011-05-22 13:33:32 CEST"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
